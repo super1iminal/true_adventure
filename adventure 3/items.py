3fdab8fda28ad497bd76ecec73a6b21b3038fda8
super1iminal <asher.arya@gmail.com>
2022-05-01 20:41:28 -0400

    items

--- adventure 3/items.py
+++ adventure 3/items.py
@@ -1,1 +1,10 @@
-class items:
+class Items(object):
+    def __init__(self):
+        pass
+    pass
+    class Consumables(Items):
+        def __init__(self, hp_regen, hp_instant_heal):
+            self.hp_regen = hp_regen
+        pass
+        class Potions(Consumables):
+    
