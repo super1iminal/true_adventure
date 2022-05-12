ca93afe9e2d295210d7f21482d86ee95ceb4e73e
super1iminal <asher.arya@gmail.com>
2022-05-12 15:13:48 -0400

    wat

--- adventure 3/player.py
+++ adventure 3/player.py
@@ -3,11 +3,13 @@ from items import *
 
 class Player(object):
     """Class representing a player"""
-    def __init__(self, hp = 10, damage = 1):
+    def __init__(self, username='Player', hp = 10, damage = 1):
+        self.name = username
         self.maxhealth = hp
         self.health = self.maxhealth
         self.bdamage = damage #base damage
         self.inventory = Inventory()
+        self.enemies_killed = 0
 
     def levelUp(self):
         self.maxhealth += 1
@@ -25,3 +27,9 @@ class Player(object):
             return True
         else:
             return False
+
+    def killed_enemy(self):
+        self.enemies_killed += 1
+
+    def __str__(self):
+        rep = '| {} | Max Health: {} | Current Health: {} | Base Damage: {} | Total Damage with Weapons: {} |'.format(self.name, self.maxhealth, self.health, self.bdamage, Player.damage_dealt())
