5d8bdfa99efd46333019f3fe6b7ba17c7372ffc1
Aitchel43 <aitchandel43@gmail.com>
2022-05-13 16:11:19 -0400

    gsfs

--- adventure 3/item_gen.py
+++ adventure 3/item_gen.py
@@ -1,8 +1,27 @@
-from items import *
 from random import randint
 
-adj = open ('adjectives.txt')
-
-def item_gen(self):
+def adjective_gen():
+    adjectives = ''
+    adj = open ('words/adjectives.txt')
+    adjective1 = adj.readline().strip().split()
     if randint(0,1) == 1:
-        self.name = randint(adjectives)
+        adjectives += adjective1[randint(0, len(adjective1)-1)]
+    else:
+        adjectives += adjective2[randint(0, len(adjective2)-1)]
+    adj.close()
+    return adjectives
+
+def weapon_gen():
+    weapon_name = ''
+    weapon = open('words/weapons.txt')
+    weap = weapon.readline().strip('\n').split('-')
+    adjectives = adjective_gen()
+    if randint(0,1) == 1:
+        weapon_name += adjectives
+        weapon_name += weap[randint(0, len(weap)-1)]
+
+adjective_gen()
+
+#def armor_gen():
+    
+
