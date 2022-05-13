41d5a990e90b790e08325e23d6f3b0af9c229091
torabi2310 <petertorabi@hotmail.com>
2022-05-13 10:52:54 -0400

    making progress!

--- adventure 3/game.py
+++ adventure 3/game.py
@@ -1,2 +1,24 @@
 #here is where you'll get all the inputs and interpret them
 #MAKE IT SO PLAYER CAN'T MOVE IF THERI BACKPACK IS OVER CAPACITY (use backpack.is_over() backpack method)
+
+print("\nWelcome to our ADVENTURE GAME")
+print("PROCEED WITH CAUTION ( ͡° ͜ʖ ͡°)")
+print("\ntype HELP if ever you need help!")
+while True:
+    action = input("\nWhat would you like to do?") 
+
+    if action == "help": 
+        helper = '''
+        move: DIRECTION (n,e,s,w)
+        e.g. move n
+
+        equip: ITEM
+        e.g. equip axe
+        --> on: LOCATION
+            e.g. on left hand
+            
+
+
+        '''
+        print(helper)
+    
