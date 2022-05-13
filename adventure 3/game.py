f38aac2df0ed9d8e23cc8b604ba7e6e6b605bb4e
torabi2310 <petertorabi@hotmail.com>
2022-05-13 12:01:36 -0400

    prog!

--- adventure 3/game.py
+++ adventure 3/game.py
@@ -1,18 +1,20 @@
 #here is where you'll get all the inputs and interpret them
-#MAKE IT SO PLAYER CAN'T MOVE IF THERI BACKPACK IS OVER CAPACITY (use backpack.is_over() backpack method)
+
 
 print("\nWelcome to our ADVENTURE GAME")
 print("PROCEED WITH CAUTION ( ͡° ͜ʖ ͡°)")
 print("\nyell HELP quick instructions!")
-
-commands = ["move", "equip", "attack", "loot", "on", "HELP"]
+ 
+mcommands = ["move", "equip", "attack", "loot"] #main commands
 while True:
     action = input("\nWhat would you like to do? ") 
 
-    if not action_s[0] in commands:
+    action_s = action.split()
+    if not action_s[0] in mcommands:
         print("Invalid command")
+        continue
 
-    if action == "HELP": 
+    if action == "HELP": #prints instructions for player
         helper = '''
 
         list of commands: [move, equip, attack, loot]
@@ -42,7 +44,88 @@ while True:
         continue
 
 
-    action_s = action.split()
+
+    if action_s[0] == "move":
+        #CHECK IF BACKPACK FULL, THEN CANT MOVE  backpack.is_over() backpack method)?
+
+
+        if action == "move": 
+            dir = input("In which direction do you move? ")
+            if dir in ["n","e","s","w"]:
+                pass#EXECUTE MOMEMENT IN GIVEN DIRECTION NEED CODE
+                
+            else: 
+                print("Invalid direction")
+                continue
+        
+        if action_s[1] in ["n","e","s","w"]:
+            pass#EXECUTE MOVEMENT IN GIVEN DIRECTION NEED CODE
+               
+        else:
+            print("Invalid direction")
+            continue
+
+
+
+
+    if action_s[0] == "loot":
+        if action == "loot":
+            print("x") #CODE TO LIST ALL POSSIBLE LOOTABLE THINGS
+            lootc = input("What would you like to loot? ")
+            #CHECK IF INPUT IS VALID THEN EXCUTE LOOT ACTION
+            continue
+
+        lootable = " ".join(action_s[1:])
+        #CHECK IF LOOTABLE THING IS VALID THEN EXECUTE LOOT
+        continue
+
+
+
+
+    if action_s[0] == "attack":
+        if action == "attack":
+            print('x') #CODE TO LIST ALL POSSIBLE ATTACKIBLES
+            attackc = input("What would you like to attack? ")
+            #CHECK IF INPUT IS VALID THEN EXCUTE ATTACK ACTION
+            continue
+
+        attackable = " ".join(action_s[1:])
+        #CHECK IF LOOTABLE THING IS VALID THEN EXECUTE ATTACK
+        continue
+
+    if action_s[0] == "equip":
+        if action == "equip":
+            print("x") #CODE TO LIST ALL POSSIBLE EQUIPABLE THINGS
+            equipc = input("What would you like to equip? ")
+            #CHECK IF INPUT IS VALID
+            print("y") #LIST ALL EQUIPABLE LOCATIONS
+            equipl = input("Where do you equip", equipc, "? ")
+            #CHECK IF INPUT IS VALID THEN EXCUTE EQUIP ACTION
+
+
+
+
+            
+        
+
+
+
+
+        
+        
+        
+
+
+
+
+
+        
+
+
+    
+
+
+    
 
     
     
